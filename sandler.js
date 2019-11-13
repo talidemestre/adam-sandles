24c6e28560666855c0f4d52c4b8610cfcec5a5e5
u6980736 <u6980736@anu.edu.au>
2019-11-13 23:23:53 +1100

    speech

--- sandler.js
+++ sandler.js
@@ -1,8 +1,20 @@
 
 speechWobble(true);
 STRING="shampoo is better, I go on first and clean the hair";
-DISPLAYSTRING = "";
-DISPLAYSTRINGCOUNT = 0;
+stringDex=0
+actOneStrings =
+    [("hey nice to meet you, i'm the sand man", 100)
+        , "I'll be your guide on this here journey today"
+        , "you don't believe i'm the real deal?"
+        , "?"
+        , "??"
+        , "???"
+        , "hold on, let me recite one of my famous adam sandler joke lines"
+        , "shampoo is better, I go on first and clean the hair"
+        , "conditioner is better, i leave the hair silky and smooth"
+    ]
+displayString = "";
+displayStringCount = 0;
 BEGIN_ANIMATE = true;
 
 
@@ -24,24 +36,30 @@ function initiateConversation(){
 //Animates the text.
 /*function animateText(string){
 	var stringArray = string.split("");
-	DISPLAYSTRING += stringArray[DISPLAYSTRINGCOUNT];
-	DISPLAYSTRINGCOUNT+=1;
-	document.getElementById("speechText").innerHTML = DISPLAYSTRING;
+	displayString += stringArray[displayStringCount];
+	displayStringCount+=1;
+	document.getElementById("speechText").innerHTML = displayString;
 }*/
 
 //Animates the text
 $('#animate').hover(function(e){
-    toSpeech = window.setInterval(doStuff, 100)
+    toSpeech = window.setInterval(printText, 100)
     var elem = document.getElementById("speechText")
     var pos = parseInt(elem.style.top)
-    var stringArray = STRING.split("");
+    var stringArray = actOneStrings[stringDex].split("");
     //console.log(pos)
-    function doStuff(){
-        if (stringArray[DISPLAYSTRINGCOUNT] != undefined){
-            DISPLAYSTRING += stringArray[DISPLAYSTRINGCOUNT];
-            DISPLAYSTRINGCOUNT+=1;
-            elem.innerHTML = DISPLAYSTRING;
-        }else{STRING = "conditioner is better, i leave the hair silky and smooth";stringArray = STRING.split("");DISPLAYSTRINGCOUNT=0; DISPLAYSTRING=""}
+    function printText(){
+        if (stringArray[displayStringCount] != undefined){
+            displayString += stringArray[displayStringCount];
+            displayStringCount+=1;
+            elem.innerHTML = displayString;
+        }
+        else{
+            stringDex ++;
+            displayStringCount = 0;
+            displayString = "";
+            stringArray = actOneStrings[stringDex].split("");
+        }
     }
 }, function(e){window.clearInterval(toSpeech);
 
@@ -58,8 +76,8 @@ function pauseAudio(id){
     var oAudio = document.getElementById(id);
     oAudio.pause();
 }
-//This script moves adam sandlers  (using jQuery)
 
+//This script moves adam sandlers  (using jQuery)
 $('#animate').hover(function(e){
     to = window.setInterval(doStuff, 5)
     var elem = document.getElementById("animate")
@@ -78,26 +96,7 @@ $('#animate').hover(function(e){
 })
 
 
-/*function moveAdam(condition) {
-    var elem = document.getElementById("animate");
-    var pos = elem.style.top;
-    var id = setInterval(frame,5);
-	var i = 0;
-    function frame(){
-        if(i>400){
-	        clearInterval(id);
-			return
-	    }else{
-		    pos++;
-	        elem.style.top = pos + "px";
-			i++;
-        }
-    }
-}
-*/
 //This script makes adam sandler talk
-
-
 $('#speechBubble').hover(function(e){
     toMouth = window.setInterval(doStuff, 100)
     var elem = document.getElementById("animateMouth")
@@ -121,28 +120,7 @@ $('#speechBubble').hover(function(e){
 
 })
 
-/*function talkAdam(condition){
-	var elem = document.getElementById("animateMouth")
-	var pos = elem.style.top;
-	var id =setInterval(frame2,100);
-	var upDown= true;
-	function frame2(){
-	    if (condition==false){
-		    clearInterval(id);
-	}else{
-	    if (upDown){
-		pos+=8;
-		upDown=false;
-		}else{
-		pos-=8
-		upDown = true;
-		}
-		elem.style.top = pos + "px";
-	}
-	}
-}
 
-*/
 //This wobbles the text box
 function speechWobble(condition) {
     console.log(condition)
@@ -168,12 +146,8 @@ function speechWobble(condition) {
     }
 }
 
-//This function changes the heading near the top of the page
-function  test(){
-    var userInput = document.getElementById("userInput").value;
-    document.getElementById("demo").innerHTML = userInput;
-}
 
+// Konami Code
 if ( window.addEventListener ) {
     var state = 0, konami = [38,38,40,40,37,39,37,39,66,65];
     window.addEventListener("keydown", function(e) {
