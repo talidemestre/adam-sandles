8962ca59d7bf83758cdbd10b51ac5436aa1e9656
u6980736 <u6980736@anu.edu.au>
2019-11-15 12:39:41 +1100

    yes/no buttons

--- sandler.js
+++ sandler.js
@@ -1,7 +1,16 @@
 
 speechWobble(true);
 STRING="shampoo is better, I go on first and clean the hair";
-stringDex=0
+stringDex=20;
+
+
+currentAudio = "voice";
+displayString = "";
+displayStringCount = 0;
+
+BEGIN_ANIMATE = true;
+ALLOW_TALK = true;
+
 actOneStrings =
     [{text:"hey nice to meet you, i'm the sand man", timeout:100, audio:'voice'}
         , {text:"I'll be your guide on this here journey today", timeout:100}
@@ -14,17 +23,20 @@ actOneStrings =
         , {text:"conditioner is better, i leave the hair silky and smooth", timeout:100}
         , {text:"oh really fool?", timeout:100}
         , {text:"really?", timeout:50}
-        , {text:"", timeout:5000}
+        , {text:"", timeout:5300}
         , {text:"stop looking at me swan", timeout:60}
         , {text:"there we go, surely you believe me now", timeout:100, audio:"voice"}
-        , {text:"", timeout:100}
+        , {text:"           ", timeout:100}
+        , {text:"look kid, ill cut the dice    ", timeout:100}
+        , {text:"i may be funny man adam sandler but i can get serious", timeout:100}
+        , {text:"i need your help", timeout:100}
+        , {text:"i need to figure out who imprisoned me in this website", timeout:100}
+        , {text:"and you're the only one I can trust", timeout:100}
+        , {text: "oh its because you're an outsider", timeout:100}
+        , {text:"so are you in?", timeout:100}
+        , {text:"Excellent!", timeout:100,  action:helpButtons}
+
     ]
-
-currentAudio = "voice";
-displayString = "";
-displayStringCount = 0;
-BEGIN_ANIMATE = true;
-
 
 // Setup for autoplay vs non autoplay
 document.getElementById("talkButton").style.visibility = "hidden";
@@ -34,6 +46,24 @@ document.getElementById("player").play().catch(function(error) {
     document.getElementById("speechElems").style.visibility = "hidden";
     BEGIN_ANIMATE = false;
 } )
+
+function helpButtons() {
+    BEGIN_ANIMATE = false;
+    ALLOW_TALK = false;
+    document.getElementById("trueFalseButtons").style.visibility = "visible";
+    document.getElementById(currentAudio).pause()
+
+}
+
+function acceptQ(){
+    BEGIN_ANIMATE = true;
+    ALLOW_TALK = true;
+    document.getElementById("trueFalseButtons").style.visibility = "hidden";
+}
+
+function denyQ(){
+    window.location = "omfgdogs.html";
+}
 
 function initiateConversation(){
     document.getElementById("speechElems").style.visibility = "visible";
@@ -49,35 +79,40 @@ function initiateConversation(){
 	document.getElementById("speechText").innerHTML = displayString;
 }*/
 
-//Animates the text
+//Animates the text typewriter effect
 $('#animate').hover(function(e){
     toSpeech = window.setInterval(printText, actOneStrings[stringDex].timeout)
     var elem = document.getElementById("speechText")
     var pos = parseInt(elem.style.top)
     var stringArray = actOneStrings[stringDex].text.split("");
     //console.log(pos)
-    function printText(){
-        if (stringArray[displayStringCount] != undefined){
-            displayString += stringArray[displayStringCount];
-            displayStringCount+=1;
-            elem.innerHTML = displayString;
-        }
-        else{
-            stringDex ++;
-            displayStringCount = 0;
-            displayString = "";
-            stringArray = actOneStrings[stringDex].text.split("");
-            window.clearInterval(toSpeech);
-            if (actOneStrings[stringDex].audio != undefined) {
-                var oldAudio = document.getElementById(currentAudio);
-                currentAudio = actOneStrings[stringDex].audio;
-                var newAudio = document.getElementById(currentAudio);
-                newAudio.play()
-                oldAudio.pause()
+    function printText() {
+        if (ALLOW_TALK == true) {
+            if (stringArray[displayStringCount] != undefined) {
+                displayString += stringArray[displayStringCount];
+                displayStringCount += 1;
+                elem.innerHTML = displayString;
+            } else {
+                stringDex++;
+                displayStringCount = 0;
+                displayString = "";
+                stringArray = actOneStrings[stringDex].text.split("");
+                window.clearInterval(toSpeech);
+                if (actOneStrings[stringDex].audio != undefined) {
+                    var oldAudio = document.getElementById(currentAudio);
+                    currentAudio = actOneStrings[stringDex].audio;
+                    var newAudio = document.getElementById(currentAudio);
+                    newAudio.play()
+                    oldAudio.pause()
 
 
+                }
+                if (actOneStrings[stringDex].action != undefined) {
+                    actOneStrings[stringDex].action();
+                    window.clearInterval(toSpeech);
+                }
+                toSpeech = window.setInterval(printText, actOneStrings[stringDex].timeout)
             }
-            toSpeech = window.setInterval(printText, actOneStrings[stringDex].timeout)
         }
     }
 }, function(e){window.clearInterval(toSpeech);
@@ -168,11 +203,11 @@ function speechWobble(condition) {
 
 // Konami Code
 if ( window.addEventListener ) {
-    var state = 0, konami = [38,38,40,40,37,39,37,39,66,65];
+    var state = 0, konami = [38,38,40,40,37,39,37,39,66,65,13];
     window.addEventListener("keydown", function(e) {
         if ( e.keyCode == konami[state] ) state++;
         else state = 0;
-        if ( state == 10 )
+        if ( state == 11 )
             window.location = "omfgdogs.html";  //you can write your own code here
     }, true);
 }
